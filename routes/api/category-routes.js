997fb1fee618e080bda40bb4737b79333e0e9327
adaking1 <137830553+adaking1@users.noreply.github.com>
2023-10-20 12:06:04 -0400

    added some routes and fixed model foreign keys

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,17 +3,45 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  try{
+    const categoryData = await Category.findAll();
+    res.status(200).json(categoryData);
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
   // find all categories
   // be sure to include its associated Products
+  // TRY USING "include:[{}]"
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+  try{
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product}]
+    });
+    if (!categoryData) {
+      res.status(404).json({message: 'No category with this id!'});
+      return;
+    }
+    res.status(200).json(categoryData);
+  }
+  catch (err) {
+    res.status(404).json({message: 'No category with this id!'})
+  }
   // find one category by its `id` value
   // be sure to include its associated Products
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+  try{
+    const newCategory = await Category.create(req.body);
+    res.json(200).json(newCategory);
+  }
+  catch (err) {
+    res.json(500).json(err);
+  }
   // create a new category
 });
 
