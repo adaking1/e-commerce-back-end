ea774fabd02d7c93ab251540ef095c022f4add9c
adaking1 <137830553+adaking1@users.noreply.github.com>
2023-10-20 11:20:30 -0400

    added associations to index in models folder

--- models/index.js
+++ models/index.js
@@ -3,20 +3,26 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+const sequelize = require('../config/connection');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: 'category_id'
-})
+});
 
 // Categories have many Products
-Category.hasMany(Product, {
-  foreignKey: 'product_id'
-})
+Category.hasMany(Product);
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag
+});
+
 
 module.exports = {
   Product,
